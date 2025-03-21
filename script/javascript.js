28ebd505d2c0ce81f9e91c431e481ffe5e383aa6
mariavitoriasouzadev <maria.vitoria.de.ss@gmail.com>
2025-03-21 14:29:28 -0300

    Parte 4 - Melhorias no design e interatividade

---
+++ script/javascript.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById("adoptBtn");
+
+    button.addEventListener("click", function() {
+        alert("Parabéns! Você deu o primeiro passo para adotar um amigo peludo! 🐶💛");
+    });
+});
